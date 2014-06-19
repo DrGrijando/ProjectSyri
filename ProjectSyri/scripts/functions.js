502a5d7dfb7e4abd69923b96644ea106ed572d96
Ruben Gomez <rubengp64@gmail.com>
2014-06-19 02:45:46 -0500

    +++ DIstinction when adding vaccine/prescription to a list

--- ProjectSyri/scripts/functions.js
+++ ProjectSyri/scripts/functions.js
@@ -1,15 +1,40 @@
+var isVaccineTabSelected = true;
+
 function addVaccine(){
-    var auxString = document.getElementById("new-vaccine-title").value;
+    var auxString;
     var newItem = document.createElement("LI"); // The element type to create must be on capital letters
+    
+    if(isVaccineTabSelected == true)
+    {
+        auxString = document.getElementById("new-vaccine-title").value;
+    }
+    else    
+    {
+        auxString = document.getElementById("new-prescription-title").value;
+    }    
+    
     var newTextNode = document.createTextNode(auxString);
     newItem.appendChild(newTextNode);	// Add the TextNode to the ListItem
-    document.getElementById("vaccine-list").appendChild(newItem);	// Add the ListItem to the specified List
+    
+    if(isVaccineTabSelected == true)
+    {
+        document.getElementById("vaccine-list").appendChild(newItem);	// Add the ListItem to the specified List
+    }
+    else    
+    {
+        document.getElementById("prescription-list").appendChild(newItem);	// Add the ListItem to the specified List   
+    }
     goBack();
 }
 
 function goBack(){
     window.history.go(-1);
 }
-/*function getTitle(){
-    auxString = document.getElementById('new-vaccine-title').value;
-}*/
+
+function activateVaccines(){
+    isVaccineTabSelected = true;
+}
+
+function activatePrescriptions(){
+    isVaccineTabSelected = false;
+}
