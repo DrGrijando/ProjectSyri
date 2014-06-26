f99226433afd0a518ee592c0fa0d5fac15201571
Ruben Gomez <rubengp64@gmail.com>
2014-06-26 11:20:43 -0500

    The function for adding a new vaccine or prescription has been separated into 2 functions.
    This has been done because the code was running properly on the emulator, but not on real devices. The counterpart of separating into different functions is, in addition of having 2 almost equal functions, we need 2 layouts (one for each "adding" screen).

--- ProjectSyri/scripts/functions.js
+++ ProjectSyri/scripts/functions.js
@@ -1,6 +1,16 @@
 var isVaccineTabSelected = true;
 
-function addVaccine(){
+/* FUNCTIONS FOR ADDING A NEW VACCINE OR PRESCRIPTION (UNIFIED) */
+
+function activateVaccines(){
+    isVaccineTabSelected = true;
+}
+
+function activatePrescriptions(){
+    isVaccineTabSelected = false;
+}
+
+function addElement(){
     var auxString;
     var newItem = document.createElement("LI"); // The element type to create must be on capital letters
     
@@ -27,14 +37,25 @@ function addVaccine(){
     goBack();
 }
 
+/* FUNCTIONS FOR ADDING A NEW VACCINE OR PRESCRIPTION (SEPARATED) */
+
+function addVaccine(){
+    var newItem = document.createElement("LI"); // The element type to create must be on capital letters      
+    var newTextNode = document.createTextNode(document.getElementById("new-vaccine-title").value);
+    newItem.appendChild(newTextNode);	// Add the TextNode to the ListItem
+    document.getElementById("vaccine-list").appendChild(newItem);	// Add the ListItem to the specified List
+    goBack();
+}
+
+function addPrescription(){
+    var newItem = document.createElement("LI"); // The element type to create must be on capital letters    
+    var newTextNode = document.createTextNode(document.getElementById("new-prescription-title").value);
+    newItem.appendChild(newTextNode);	// Add the TextNode to the ListItem
+    document.getElementById("prescription-list").appendChild(newItem);	// Add the ListItem to the specified List   
+    goBack();
+}
+
 function goBack(){
     window.history.go(-1);
 }
 
-function activateVaccines(){
-    isVaccineTabSelected = true;
-}
-
-function activatePrescriptions(){
-    isVaccineTabSelected = false;
-}
