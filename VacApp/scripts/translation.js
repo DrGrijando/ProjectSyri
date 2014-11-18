86ae7428a602f215b64c9865f8f18ecbb596a66d
Ruben Gomez <rubengp64@gmail.com>
2014-11-18 16:37:57 +0000

    * Completed translation of normal text. Next step: select tags and placeholder.

--- VacApp/scripts/translation.js
+++ VacApp/scripts/translation.js
@@ -14,7 +14,26 @@ var english = kendo.observable({
     "Prescripciones" : "Prescriptions",
     "Añadir_prescripcion" : "Add prescription...",
     "Historial" : "Record",
-    "Añadir_PHR" : "Add PHR..."
+    "Añadir_PHR" : "Add PHR...",
+    "Nueva_vacuna" : "New vaccine",
+    "Nueva_vacuna_sub" : "Add a new vaccine to your schedule by introducing the following data.",
+    "Notificame" : "Notify me",
+    "Antes_de_la_hora" : "before the given hour.",
+    "Nueva_prescripcion" : "New prescription",
+    "Nueva_prescripcion_sub" : "Add a new prescription for a recent vaccine or medication.",
+    "Nuevo_Phr" : "New PHR",
+    "Nuevo_Phr_sub" : "Add a new personal history record.",
+    "Vacuna" : "Vaccine",
+    "Ver_vacuna" : "See all the information of your vaccines selecting them on the list.",
+    "Prescripcion" : "Prescription",
+    "Ver_prescripcion" : "See all the information of your prescriptions selecting them on the list.",
+    "Phr" : "PHR",
+    "Ver_Phr" : "See all the information of your Personal History Records selecting them on the list.",
+    "Guardar" : "Save",
+    "Cancelar" : "Cancel",
+    "Mover_al_historial" : "Move to record",
+    "Restaurar" : "Restore"
+
 });
 
 var spanish = kendo.observable({
@@ -33,7 +52,25 @@ var spanish = kendo.observable({
     "Prescripciones" : "Prescripciones",
     "Añadir_prescripcion" : "Añadir prescripción...",
     "Historial" : "Historial",
-    "Añadir_PHR" : "Añadir PHR..."
+    "Añadir_PHR" : "Añadir PHR...",
+    "Nueva_vacuna" : "Nueva vacuna",
+    "Nueva_vacuna_sub" : "Añada una nueva vacuna a su lista introduciendo los siguientes datos.",
+    "Notificame" : "Notifícame",
+    "Antes_de_la_hora" : "antes de la hora indicada.",
+    "Nueva_prescripcion" : "Nueva prescripción",
+    "Nueva_prescripcion_sub" : "Añada una nueva prescripción para una vacuna reciente o un tratamiento.",
+    "Nuevo_Phr" : "Nuevo PHR",
+    "Nuevo_Phr_sub" : "Añada un nuevo registro personal al historial.",
+    "Vacuna" : "Vacuna",
+    "Ver_vacuna" : "Vea toda la información de sus vacunas seleccionándolas en la lista.",
+    "Prescripcion" : "Prescripción",
+    "Ver_prescripcion" : "Vea toda la información de sus prescripciones seleccionándolas en la lista.",
+    "Phr" : "PHR",
+    "Ver_Phr" : "Vea toda la información de sus registros personales seleccionándolos en la lista.",
+    "Guardar" : "Guardar",
+    "Cancelar" : "Cancelar",
+    "Mover_al_historial" : "Mover al historial",
+    "Restaurar" : "Restaurar"
 });
 
 var catalan = kendo.observable({
@@ -52,7 +89,25 @@ var catalan = kendo.observable({
     "Prescripciones" : "Prescripcions",
     "Añadir_prescripcion" : "Afegir prescripció...",
     "Historial" : "Historial",
-    "Añadir_PHR" : "Afegir PHR..."
+    "Añadir_PHR" : "Afegir PHR...",
+    "Nueva_vacuna" : "Nova vacuna",
+    "Nueva_vacuna_sub" : "Afegeixi una nova vacuna a la seva llista introduint les dades següents.",
+    "Notificame" : "Notifica'm",
+    "Antes_de_la_hora" : "abans de l'hora indicada.",
+    "Nueva_prescripcion" : "Nova prescripció",
+    "Nueva_prescripcion_sub" : "Afegeixi una nova prescripció per a una vacuna recent o un tractament.",
+    "Nuevo_Phr" : "Nou PHR",
+    "Nuevo_Phr_sub" : "Afegeixi un nou registre personal a l'historial.",
+    "Vacuna" : "Vacuna",
+    "Ver_vacuna" : "Vegi tota la informació de les seves vacunes seleccionant-les a la llista.",
+    "Prescripcion" : "Prescripció",
+    "Ver_prescripcion" : "Vegi tota la informació de les seves prescripcions seleccionant-les a la llista.",
+    "Phr" : "PHR",
+    "Ver_Phr" : "Vegi tota la informació dels seus seves registres personals seleccionant-los a la llista.",
+    "Guardar" : "Guardar",
+    "Cancelar" : "Cancel·lar",
+    "Mover_al_historial" : "Moure a l'historial",
+    "Restaurar" : "Restaurar"
 });
 
 var dict = {
